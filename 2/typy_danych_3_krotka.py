6fbbd1444a8926f211a3801d39d4e7636bc67670
rajkonkret <rajkonkret660@gmail.com>
2024-09-17 14:20:09 +0200

    [2.14] update typy_danych_3_krotka.py

--- 2/typy_danych_3_krotka.py
+++ 2/typy_danych_3_krotka.py
@@ -1,6 +1,8 @@
 # krotka (tupla) - kolekcja niemutowalna
 # pozwala efektywniej zarządzać pamięcią
 # krotka jednoelementowa - stała - zmienna
+from time import perf_counter
+from traceback import print_tb
 
 tupla = ("Radek")
 print(type(tupla))  # <class 'str'>
@@ -29,3 +31,41 @@ print(type(tupla_imiona))  # <class 'tuple'>
 
 print(tupla_imiona.count("Radek"))  # wystepuje 1 raz
 print(tupla_imiona.index("Mariusz"))  # index numer 4
+
+# rozpakowanie tupli (krotki)
+tup = 1, 2
+print(type(tup))  # <class 'tuple'>
+
+a, b = 1, 2
+print(a, b)  # 1 2
+a, b = b, a
+print(a, b)  # 2 1
+a, b = tup
+print(a, b)  # 1,2
+# a, b = 1, 2, 3# ValueError: too many values to unpack (expected 2)
+tup_3_elementy = 1, 2, 3
+# a, b = tup_3_elementy
+a, *b = tup_3_elementy
+print(a, b)  # 1 [2, 3] * worek na pozostale dane
+*a, b = tup_3_elementy
+print(a, b)  # [1, 2] 3
+
+print(tupla_imiona)
+print(len(tupla_imiona))
+
+# name1, name2, name3, name4
+print(tupla_imiona)  # ('Radek', 'Tomek', 'Zenek', 'Michał', 'Mariusz', 'Grzegorz')
+name1, *name2, name3, name4 = tupla_imiona
+print(name1, name2, name3, name4)
+# Radek ['Tomek', 'Zenek', 'Michał'] Mariusz Grzegorz
+name1, name2, *name3, name4 = tupla_imiona
+print(name1, name2, name3, name4)
+# Radek Tomek ['Zenek', 'Michał', 'Mariusz'] Grzegorz
+
+# sortowanie krotki zwraca listę
+print(sorted(tupla_imiona))  # ['Grzegorz', 'Mariusz', 'Michał', 'Radek', 'Tomek', 'Zenek']
+print(tupla_imiona)  # ('Radek', 'Tomek', 'Zenek', 'Michał', 'Mariusz', 'Grzegorz')
+
+lista = list(tupla_imiona)
+print(lista)  # ['Radek', 'Tomek', 'Zenek', 'Michał', 'Mariusz', 'Grzegorz']
+print(type(lista))  # <class 'list'>
