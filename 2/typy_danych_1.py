f81205145240f96ea8ebc03f649f2fb4398cf3be
rajkonkret <rajkonkret660@gmail.com>
2024-09-17 11:46:52 +0200

    [2.7]  update typy_danych_1.py

--- 2/typy_danych_1.py
+++ 2/typy_danych_1.py
@@ -109,4 +109,13 @@ print(False or False)  # False
 print(not True)  # False
 print(not False)  # True
 
+a = 8
+b = 6
 
+print(f"Porównanie {a} > {b} = {a > b}")  # Porównanie 8 > 6 = True
+print(f"Porównanie {a} < {b} = {a < b}")  # Porównanie 8 < 6 = False
+print(f"Porównanie {a <= b=}")  # Porównanie a <= b=False
+print(f"Porównanie {a >= b=}")  # Porównanie a >= b=True
+print(f"Porównanie {a} == {b} = {a == b}")  # Porównanie 8 == 6 = False, == - prównanie
+print(f"Porównanie {a} != {b} = {a != b}")  # czy różne, Porównanie 8 != 6 = True
+print(f"{a=}")  # a=8
