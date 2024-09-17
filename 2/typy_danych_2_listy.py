616c940b6c96c8a4020d2f76fe1af5d81caa15b1
rajkonkret <rajkonkret660@gmail.com>
2024-09-17 12:20:36 +0200

    [2.9] update typy_danych_2_listy.py

--- 2/typy_danych_2_listy.py
+++ 2/typy_danych_2_listy.py
@@ -31,5 +31,29 @@ print(len(lista))  # 7
 # 7 - 1
 print(len(lista) - 1)
 print(lista[-1])  # Malwina, ostatni elemnt z listy
-print(lista[-3]) # Mariusz
-print()
+print(lista[-3])  # Mariusz
+print(lista[-2])  # Ja
+
+# wyswietlanie fragmentu listy (slicownie)
+print(lista[0:3])  # start:stop -> 012 zbior otwarty, ['Radek', 'Tomek', 'Zenek']
+print(lista[:3])  # ['Radek', 'Tomek', 'Zenek']
+print(lista[2:])  # ['Zenek', 'Grzegorz', 'Mariusz', 'Ja', 'Malwina'] 2,3,4,5,6
+print(lista[2:6])  # ['Zenek', 'Grzegorz', 'Mariusz', 'Ja'] 2,3,4,5
+print(lista[2:7])  # ['Zenek', 'Grzegorz', 'Mariusz', 'Ja', 'Malwina']
+print(lista[2:15])  # ['Zenek', 'Grzegorz', 'Mariusz', 'Ja', 'Malwina']
+print(lista[:])  # ['Radek', 'Tomek', 'Zenek', 'Grzegorz', 'Mariusz', 'Ja', 'Malwina']
+print(lista[2:3])  # ['Zenek']
+print(lista[2:2])  # []
+print(lista[10:29])  # []
+
+# ['Radek', 'Tomek', 'Zenek', 'Grzegorz', 'Mariusz', 'Ja', 'Malwina']
+#     0         1        2        3           4        5       6
+#    -7        -6       -5       -4          -3       -2       -1
+print(lista[-2:0])  # [-2:-7], [4:0] -> []
+print(lista[0:-2])  # ['Radek', 'Tomek', 'Zenek', 'Grzegorz', 'Mariusz']
+
+lista_15 = list(range(15))  # 0 do 14
+print(lista_15)  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+print(lista_15[0:15:2])  # start:stop:krok, [0, 2, 4, 6, 8, 10, 12, 14]
+print(lista_15[-10])  # 5
+print(list(range(0, 15, 2)))  # [0, 2, 4, 6, 8, 10, 12, 14]
