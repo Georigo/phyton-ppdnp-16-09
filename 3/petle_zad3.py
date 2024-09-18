269841a8913836fcef2e7bfd8dca085e4cee1428
rajkonkret <rajkonkret660@gmail.com>
2024-09-18 15:33:25 +0200

    [3.14] uodate petle_zad3.py

--- 3/petle_zad3.py
+++ 3/petle_zad3.py
@@ -51,3 +51,10 @@ print(lista)  # ['1', '5', '78', '90', '000']
 print(lista_int)
 # ['1', '2', '3', '4', '5', '6']
 # [1, 2, 3, 4, 5, 6]
+
+my_list = [1, 5, 2, 3, 5, 4, 5, 6, 5]
+element_to_remove = 5
+while element_to_remove in my_list:
+    my_list.remove(element_to_remove)
+
+print(my_list)  # [1, 2, 3, 4, 6]
