a790e35f90dd3978df262e5d8d7179f1367f736d
rajkonkret <rajkonkret660@gmail.com>
2024-09-18 13:17:04 +0200

    [3.9] add petle_zad1.py

---
+++ 3/petle_zad1.py
@@ -0,0 +1,34 @@
+# pętle - dająmożliwośc wykonania wielokrotnie tego samego kodu
+# for - pętla iteracyjna
+
+for i in range(5):  # od 0 do 4
+    print(i)
+
+for i in range(10):
+    pass  # nic nie rób
+
+for _ in range(10):  # niema zmienna
+    print("Test podłoga")
+    print(_)
+# Test podłoga
+# 0
+# Test podłoga
+# 1
+# Test podłoga
+# 2
+# Test podłoga
+# 3
+# Test podłoga
+# 4
+# Test podłoga
+# 5
+# Test podłoga
+# 6
+# Test podłoga
+# 7
+# Test podłoga
+# 8
+# Test podłoga
+# 9
+for i in range(20):
+    print(i * 2)
