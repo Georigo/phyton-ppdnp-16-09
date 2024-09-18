0abcb7f1fc46e84e4e0ded90554820d1e662ee0a
rajkonkret <rajkonkret660@gmail.com>
2024-09-18 12:02:55 +0200

    [3.6] update warunki2.py

--- 3/warunki2.py
+++ 3/warunki2.py
@@ -5,7 +5,7 @@ lista = []
 lang = input("Podaj znany Ci język programowania")
 
 match lang.lower().strip():
-    case "pyhton":
+    case "python":
         lista.append("Znam Pythona")
     case "java":
         lista.append("Jave też znam")
@@ -13,3 +13,11 @@ match lang.lower().strip():
         print("Nie znam takiego języka")
 
 print(lista)
+
+match lang:
+    case ("python" | "python3"):  # | oznacza or czyli lub,
+        print("Znam Pythona")
+
+# Podaj znany Ci język programowaniapython
+# ['Znam Pythona']
+# Znam Pythona
