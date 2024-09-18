b4425aadfa3a9318c98927abd044385cef346247
rajkonkret <rajkonkret660@gmail.com>
2024-09-18 15:30:34 +0200

    [3.13] add petle_zad3.py

---
+++ 3/petle_zad3.py
@@ -0,0 +1,53 @@
+# while - pętla sterowana warunkiem
+
+# pętla nieskończona
+# while True:
+#     print("Komunikat 1 !")
+
+licznik = 0
+while True:
+    licznik += 1
+    print("Komunikat 1 !")
+    if licznik > 10:
+        break
+        # przerywa pętle
+
+print(licznik)  # 11
+licznik = 0
+while licznik < 10:
+    licznik += 1
+    print("Komunikat 2 !!")
+# Komunikat 2 !!
+# Komunikat 2 !!
+# Komunikat 2 !!
+# Komunikat 2 !!
+# Komunikat 2 !!
+# Komunikat 2 !!
+# Komunikat 2 !!
+# Komunikat 2 !!
+# Komunikat 2 !!
+# Komunikat 2 !!
+
+# password = input("Podaj hasło")
+# while password != "secret":
+#     password = input("Błędne hasło, podaj ponownie")
+# print("Hasło prawidłowe")
+# Podaj hasłorewrwe
+# Błędne hasło, podaj ponowniesdfs
+# Błędne hasło, podaj ponowniefsdfsd
+# Błędne hasło, podaj ponowniefsfs
+# Błędne hasło, podaj ponowniesecret
+# Hasło prawidłowe
+
+lista = []
+lista_int = []
+while True:
+    wej = input("Podaj liczbę")
+    if not wej.isnumeric():
+        break
+    lista.append(wej)
+    lista_int.append(int(wej))
+print(lista)  # ['1', '5', '78', '90', '000']
+print(lista_int)
+# ['1', '2', '3', '4', '5', '6']
+# [1, 2, 3, 4, 5, 6]
