5af97903f0feec62cd44911ca455255d0a48f081
rajkonkret <rajkonkret660@gmail.com>
2024-09-20 10:01:41 +0200

    [5.5] add fun8.py

--- 5/fun7_lambda.py
+++ 5/fun7_lambda.py
@@ -18,3 +18,26 @@ print(wiek(10))  # nastolatek
 print(wiek(17))  # nastolatek
 print(wiek(18))  # dorosły
 print(wiek(25))  # dorosły
+
+lista_wyn = []
+lista = [1, 2, 3, 10, 20, 30, 50, 70, 200, 250]
+for i in lista:
+    lista_wyn.append(i * 2)
+
+print(lista_wyn)  # [2, 4, 6, 20, 40, 60, 100, 140, 400, 500]
+print([i * 2 for i in lista])  # [2, 4, 6, 20, 40, 60, 100, 140, 400, 500]
+
+
+def zmiana(x):
+    return x * 2
+
+
+lista_wyn2 = []
+for i in lista:
+    lista_wyn2.append(zmiana(i))
+print(lista_wyn2)  # [2, 4, 6, 20, 40, 60, 100, 140, 400, 500]
+
+# funkcje wyzszego rzedu
+# map() - pobiera element, wykonuje na nim wskazaną funkcję
+print(f"Zastosowanie map(): {list(map(zmiana, lista))}")
+# Zastosowanie map(): [2, 4, 6, 20, 40, 60, 100, 140, 400, 500]
