f0c0e49e7e178f1f6c942871389cdb8ff702b645
rajkonkret <rajkonkret660@gmail.com>
2024-09-20 10:45:32 +0200

    [5.6] update fun8.py

--- 5/fun7_lambda.py
+++ 5/fun7_lambda.py
@@ -41,3 +41,31 @@ print(lista_wyn2)  # [2, 4, 6, 20, 40, 60, 100, 140, 400, 500]
 # map() - pobiera element, wykonuje na nim wskazaną funkcję
 print(f"Zastosowanie map(): {list(map(zmiana, lista))}")
 # Zastosowanie map(): [2, 4, 6, 20, 40, 60, 100, 140, 400, 500]
+# użycie lambdy jako funkcji anonimowej
+print(f"Zastosowanie map(): {list(map(lambda x: x * 2, lista))}")
+print(f"Zastosowanie map(): {list(map(lambda x: x * 4, lista))}")
+print(f"Zastosowanie map(): {list(map(lambda x: x * 12, lista))}")
+# Zastosowanie map(): [4, 8, 12, 40, 80, 120, 200, 280, 800, 1000]
+# Zastosowanie map(): [12, 24, 36, 120, 240, 360, 600, 840, 2400, 3000]
+
+# filtrowanie danych
+
+l3 = []
+for i in lista:
+    if i < 20:
+        l3.append(i)
+
+print(l3)  # [1, 2, 3, 10]
+
+# filter() - wraca tylko dane spełniające warunek
+print(f"Zastosowanie filter(): {list(filter(lambda x: x < 20, lista))}")  # Zastosowanie filter(): [1, 2, 3, 10]
+print(f"Zastosowanie filter(): {list(filter(lambda x: x > 20, lista))}")  # Zastosowanie filter(): [1, 2, 3, 10]
+print(
+    f"Zastosowanie filter(): {list(filter(lambda x: x > 5 and x < 100, lista))}")  # Zastosowanie filter(): [1, 2, 3, 10]
+# Zastosowanie filter(): [1, 2, 3, 10]
+# Zastosowanie filter(): [30, 50, 70, 200, 250]
+# Zastosowanie filter(): [10, 20, 30, 50, 70]
+print(f"Zastosowanie filter(): {list(filter(lambda x: 5 < x < 100, lista))}")
+# Zastosowanie filter(): [10, 20, 30, 50, 70]
+
+# functools
